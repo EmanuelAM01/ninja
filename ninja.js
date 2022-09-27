c88b8e5e521418f18a8f88c8ba6dc6165ae6f9ed
Emanuel Aguirre Molina <emagui11572@gmail.com>
2022-09-26 20:34:19 -0500

    3 ninjas created

---
+++ ninja.js
@@ -0,0 +1,41 @@
+class Ninja{
+    constructor(nombre, salud=0){
+        this.nombre = nombre;
+        this.salud = salud;
+        this.velocidad = 3; 
+        this.fuerza = 3;
+    }
+    sayName(){
+        console.log("El Nija se llama"+" "+this.nombre);
+    }
+    showStats(){
+        console.log("El Nija se llama"+" "+this.nombre);
+        console.log("Este Nija tiene una fuerza de"+" "+this.fuerza);
+        console.log("Este Nija tiene una velocidad de"+" "+this.velocidad);
+        console.log("Este Nija tiene una salud de"+" "+this.salud);
+    } 
+    drinkSake(){
+        this.salud += 10;
+    }
+}
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.drinkSake();
+ninja1.drinkSake();
+ninja1.showStats();
+
+const ninja2 = new Ninja("Yoko", 25);
+ninja2.sayName();
+ninja2.drinkSake();
+ninja2.drinkSake();
+ninja2.drinkSake();
+ninja2.showStats();
+
+const ninja3 = new Ninja("Chihiro", 455);
+ninja3.sayName();
+ninja3.drinkSake();
+ninja3.drinkSake();
+ninja3.drinkSake();
+ninja3.showStats();
+
