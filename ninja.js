5b489a8bf1c6ce09fce4a115e691d2ee9d6f4621
Emanuel Aguirre Molina <emagui11572@gmail.com>
2022-09-26 22:12:05 -0500

    creacion de clase sensei y extension de la ninja

--- ninja.js
+++ ninja.js
@@ -24,14 +24,14 @@ ninja1.drinkSake();
 ninja1.drinkSake();
 ninja1.drinkSake();
 ninja1.showStats();
-
+console.log("-----------------------------------")
 const ninja2 = new Ninja("Yoko", 25);
 ninja2.sayName();
 ninja2.drinkSake();
 ninja2.drinkSake();
 ninja2.drinkSake();
 ninja2.showStats();
-
+console.log("-----------------------------------")
 const ninja3 = new Ninja("Chihiro", 455);
 ninja3.sayName();
 ninja3.drinkSake();
@@ -39,3 +39,4 @@ ninja3.drinkSake();
 ninja3.drinkSake();
 ninja3.showStats();
 
+module.exports = Ninja;
---
+++ sensei.js
@@ -0,0 +1,38 @@
+const Ninja = require("./Ninja");
+
+class Sensei extends Ninja {
+    constructor(nombre){
+        super(nombre, 200)
+        this.velocidad=10;
+        this.fuerza=10;
+        this.sabiduria=10; 
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.")
+        console.log("tienes +10 de salud")
+    }
+
+
+}
+console.log("-----------------------------------")
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
+console.log("-----------------------------------")
+const superSensei2 = new Sensei("Mr. Miyagui");
+superSensei2.speakWisdom();
+superSensei2.speakWisdom();
+superSensei2.speakWisdom();
+superSensei2.showStats(); //segun lo anterioir uno puede llamar a un metodo de clase del padre sin tenerlo declarado en el hijo, ya que showStas solo esta en el padre
+console.log("-----------------------------------")
+const superSensei3 = new Sensei("Mr. Yoko");
+superSensei3.speakWisdom();
+superSensei3.speakWisdom();
+superSensei3.speakWisdom();
+superSensei3.speakWisdom();
+superSensei3.speakWisdom();
+superSensei3.speakWisdom();
+superSensei3.showStats();
+
+
